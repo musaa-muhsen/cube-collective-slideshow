d0a9eec95c706fbb2b308b512c9a87aa29889aae
musaa-muhsen <musaamuhsen@gmail.com>
2021-05-05 13:16:31 +0100

    update

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-  
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
+const path = require('path');
 
-console.log(process.env.NODE_ENV)
 
-if (process.env.NODE_ENV === "production"){
-   app.use(express.static("build"));
-   app.get("*", (req, res) => {
-     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-   });
- }
+// if (process.env.NODE_ENV === "production"){
+//    app.use(express.static("build"));
+//    app.get("*", (req, res) => {
+//      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//    });
+//  }
 //console.log(cors)
 app.use(cors())
 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 
 const port = process.env.PORT || 3333;
