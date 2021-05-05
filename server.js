2ea221d2d71bfae8dbae536e887067c023fb22f4
musaa-muhsen <musaamuhsen@gmail.com>
2021-05-05 21:13:22 +0100

    update

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const path = require('path');
 //console.log(cors)
 app.use(cors())
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
