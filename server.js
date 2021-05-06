7860aaff10149eaf07bbe0314d95c0c34df3906d
musaa-muhsen <musaamuhsen@gmail.com>
2021-05-06 11:41:24 +0100

    updated

--- server.js
+++ server.js
@@ -17,8 +17,15 @@ app.use(cors())
 // app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
   });
+
+//   if (process.env.NODE_ENV === "production"){
+//     app.use(express.static("/build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//     });
+//   }
 
 
 const port = process.env.PORT || 3333;
